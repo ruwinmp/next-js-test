0ca97def3306a9739f7b99bd15b685448549d53d
ruwinmp <ruwinmp@gmail.com>
2026-09-12 23:48:20 -0700

    Route navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1>Welcome to Next.js!</h1>
+      <br />
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/product">Product</Link></li>
+        <li><Link href="/services">Services</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
     </>
   );
 }
